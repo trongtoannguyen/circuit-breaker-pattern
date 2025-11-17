85dd4a19ea143aef7ef852ef03ecbb94e4986ec6
toannt52 <toannt52@fpt.com>
2025-11-18 00:15:18 +0700

    Refactor circuit breaker to support state transitions and events
    Introduced `CircuitBreakerSwitch` and `CircuitBreakerListener` interfaces for handling state transitions and event notifications. Updated `DefaultCircuitBreaker` to manage states (`Open`, `Closed`, `Half-Open`) and notify listeners on transitions. This enhances flexibility and extensibility in managing circuit breaker behaviors.

--- src/main/java/example/circuitbreaker/CircuitBreakerSwitch.java
+++ src/main/java/example/circuitbreaker/CircuitBreakerSwitch.java
@@ -1,4 +1,29 @@
 package example.circuitbreaker;
 
+import example.circuitbreaker.states.CircuitBreakerState;
+
+/**
+ * This interface defines methods for transitioning a circuit breaker between various states.
+ * It is responsible for handling the operations of opening, closing, and attempting
+ * to close the circuit based on the current state.
+ */
 public interface CircuitBreakerSwitch {
+
+    /**
+     * Method to open the circuit breaker
+     *
+     * @param from the state from which the circuit is being opened
+     */
+    void openCircuit(CircuitBreakerState from);
+
+    /**
+     * Method to attempt to close the circuit breaker.
+     * Called when in Half-Open state and conditions to close are met.
+     */
+    void attemptToCloseCircuit(CircuitBreakerState from);
+
+    /**
+     * Method to close the circuit breaker
+     */
+    void closeCircuit(CircuitBreakerState from);
 }
--- src/main/java/example/circuitbreaker/states/OpenCircuitBreakerState.java
+++ src/main/java/example/circuitbreaker/states/OpenCircuitBreakerState.java
@@ -1,4 +1,37 @@
 package example.circuitbreaker.states;
 
-public class OpenCircuitBreakerState {
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
+
+public class OpenCircuitBreakerState implements CircuitBreakerState {
+
+    @Override
+    public void enter() {
+
+    }
+
+    @Override
+    public void invocationFails() {
+
+    }
+
+    @Override
+    public void invocationSucceeds() {
+
+    }
+
+    @Override
+    public void invoke(Runnable action) {
+
+    }
+
+    @Override
+    public <T> T invoke(Supplier<T> func) {
+        return null;
+    }
+
+    @Override
+    public <T> CompletableFuture<T> invokeAsync(Supplier<CompletableFuture<T>> func) {
+        return null;
+    }
 }

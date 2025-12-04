7889bb834efc90c7c06554a43a31d4c5aac09cc9
trongtoannguyen <trongtoan1609ht@gmail.com>
2025-12-04 18:53:15 +0700

    add unit test case for HalfOpenCircuitBreakerState with action

--- circuit-breaker/src/main/java/example/circuitbreaker/states/HalfOpenCircuitBreakerState.java
+++ circuit-breaker/src/main/java/example/circuitbreaker/states/HalfOpenCircuitBreakerState.java
@@ -15,9 +15,43 @@ public class HalfOpenCircuitBreakerState implements CircuitBreakerState {
     private final int invoking = 1;
     private final CircuitBreakerInvoker invoker;
     private final Duration timeout;
-    private AtomicInteger isBeingInvoked;
+    private AtomicInteger isBeingInvoked = new AtomicInteger(notInvoking);
     private final CircuitBreakerSwitch switcher;
 
+    public int getNotInvoking() {
+        return notInvoking;
+    }
+
+    public int getInvoking() {
+        return invoking;
+    }
+
+    public CircuitBreakerInvoker getInvoker() {
+        return invoker;
+    }
+
+    public Duration getTimeout() {
+        return timeout;
+    }
+
+    public AtomicInteger isBeingInvoked() {
+        return isBeingInvoked;
+    }
+
+    public CircuitBreakerSwitch getSwitcher() {
+        return switcher;
+    }
+
+    /**
+     * Constructs a new instance of HalfOpenCircuitBreakerState.
+     * This state represents the "half-open" condition in a circuit breaker,
+     * where a limited number of requests are allowed to execute to test
+     * if the circuit can safely transition back to the "closed" state.
+     *
+     * @param switcher the CircuitBreakerSwitch to handle transitions between states
+     * @param invoker  the CircuitBreakerInvoker to execute actions with specific timing and constraints
+     * @param timeout  the timeout duration for requests while in the half-open state
+     */
     public HalfOpenCircuitBreakerState(CircuitBreakerSwitch switcher, CircuitBreakerInvoker invoker, Duration timeout) {
         this.switcher = switcher;
         this.invoker = invoker;
--- circuit-breaker/src/test/java/example/circuitbreaker/states/HalfOpenCircuitBreakerStateTest.java
+++ circuit-breaker/src/test/java/example/circuitbreaker/states/HalfOpenCircuitBreakerStateTest.java
@@ -1,7 +1,92 @@
 package example.circuitbreaker.states;
 
-import static org.junit.jupiter.api.Assertions.*;
+import example.circuitbreaker.CircuitBreakerInvoker;
+import example.circuitbreaker.CircuitBreakerSwitch;
+import example.circuitbreaker.exceptions.CircuitBreakerOpenException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.Duration;
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+@ExtendWith(MockitoExtension.class)
 class HalfOpenCircuitBreakerStateTest {
 
+    //@formatter:off
+    private static final Duration TIMEOUT = Duration.ofMillis(100);
+    private HalfOpenCircuitBreakerState sut;
+    @Mock private CircuitBreakerInvoker invoker;
+    @Mock private CircuitBreakerSwitch switcher;
+    //@formatter:on
+
+    @BeforeEach
+    void setUp() {
+        sut = new HalfOpenCircuitBreakerState(switcher, invoker, TIMEOUT);
+    }
+
+    @Nested
+    class StateInvokeTest {
+
+        @Test
+        void invokeFirstActionOnly() {
+            Runnable action = mock(Runnable.class);
+            InOrder inOrder = Mockito.inOrder(invoker);
+
+            //#1: action is invoked here
+            sut.invoke(action);
+            assertEquals(1, sut.isBeingInvoked().get());
+
+            //#2: later action is rejected
+            assertThrows(CircuitBreakerOpenException.class, () -> sut.invoke(action));
+
+            //verify in order
+            //#1
+            inOrder.verify(invoker).invokeThrough(same(sut), same(action), eq(TIMEOUT));
+
+            //#2: no more invocations by throwing CircuitBreakerOpenException
+            inOrder.verify(invoker, never()).invokeThrough(same(sut), same(action), any());
+            verifyNoMoreInteractions(invoker);
+        }
+
+        @Test
+        void invokeFirstSupplierOnly() {
+            Supplier<String> func = mock(Supplier.class);
+            InOrder inOrder = Mockito.inOrder(invoker);
+
+            //#1: func is invoked here
+            sut.invoke(func);
+            assertEquals(1, sut.isBeingInvoked().get());
+
+            //#2: later func is rejected
+            assertThrows(CircuitBreakerOpenException.class, () -> sut.invoke(func));
+
+            //verify in order
+            //#1
+            inOrder.verify(invoker).invokeThrough(same(sut), same(func), eq(TIMEOUT));
+
+            //#2: no more invocations by throwing CircuitBreakerOpenException
+            inOrder.verify(invoker, never()).invokeThrough(same(sut), same(func), any());
+            verifyNoMoreInteractions(invoker);
+        }
+    }
+
+    @Nested
+    class StateInvokeAsyncTest {
+
+    }
 }
